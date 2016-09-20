70b3ec1e99f5c1f31bfca77ce6797d4cd7205358
Bastoche <sebastien.pereda@gmail.com>
2016-09-20 15:57:47 +0200

    Corrections Insight

--- src/AppBundle/Controller/BookingController.php
+++ src/AppBundle/Controller/BookingController.php
@@ -75,34 +75,27 @@ class BookingController extends Controller
     {
         $commande = $this->get('booking.service')->getCommande($id);
         $token = $request->request->get('stripeToken');
-        $error = false;
         if ($request->isMethod('POST')) {
-            try {
-                $this->get('stripe.service')->createCharge($commande->getAmount(), $token, $commande->getEmail());
-            } catch (\Stripe\Error\Card $e) {
-                $error = 'Un problème est survenu : '.$e->getMessage();
+            $this->get('stripe.service')->createCharge($commande->getAmount(), $token, $commande->getEmail());
+            $this->get('booking.service')->changeStatus($commande->getId());
+            $this->addFlash('success', 'Merci pour votre commande, vous allez recevoir les billets par e-mail.');
+            $message = \Swift_Message::newInstance()
+                ->setSubject('Votre réservation pour le musée du Louvre')
+                ->setFrom('reservation@louvre.com')
+                ->setTo($commande->getEmail())
+                ->setBody(
+                    $this->renderView(
+                        'email/reservation.html.twig',
+                        array(
+                            'commande' => $commande,
+                            'tickets' => $commande->getTickets()
+                        )
+                    ),
+                    'text/html'
+                );
+            $this->get('mailer')->send($message);
+            return $this->redirectToRoute('index');
             }
-            if (!$error) {
-                $this->get('booking.service')->changeStatus($commande->getId());
-                $this->addFlash('success', 'Merci pour votre commande, vous allez recevoir les billets par e-mail.');
-                $message = \Swift_Message::newInstance()
-                    ->setSubject('Votre réservation pour le musée du Louvre')
-                    ->setFrom('reservation@louvre.com')
-                    ->setTo($commande->getEmail())
-                    ->setBody(
-                        $this->renderView(
-                            'email/reservation.html.twig',
-                            array(
-                                'commande' => $commande,
-                                'tickets' => $commande->getTickets()
-                            )
-                        ),
-                        'text/html'
-                    );
-                $this->get('mailer')->send($message);
-                return $this->redirectToRoute('index');
-            }
-        }
         if ($this->get('booking.service')->getNbrTickets($commande->getVisitDate()) + $this->get('booking.service')->ticketsCommande($id) <= 1000) {
             return $this->render('booking/checkout.html.twig', array(
                 'id' => $id,
@@ -111,7 +104,6 @@ class BookingController extends Controller
                 'email' => $commande->getEmail(),
                 'amount' => $this->get('booking.service')->getAmount($id),
                 'stripe_public_key' => $this->getParameter('stripe_public_key'),
-                'error' => $error
             ));
         }
         throw new \Exception("Il n'y a pas suffisamment de places disponibles pour le jour choisi. Merci de modifier votre commande.");
